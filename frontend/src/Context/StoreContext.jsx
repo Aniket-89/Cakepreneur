b2e9668d0147c7adff01bcee0411fd8428cd5512
Aniket <aniketkumarg99@gmail.com>
2024-05-26 22:05:27 +0530

    prototype

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://44.226.145.213:4000"
+    const url = "https://cakepreneur.onrender.com/"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
