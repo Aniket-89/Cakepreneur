7b7eb5dae49853779f446ad5a0c91602f8d14b6b
Aniket <aniketkumarg99@gmail.com>
2024-05-26 20:59:16 +0530

    prototype

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    const uri = process.env.MONGODB_URI;
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
-};
+export const  connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://aniketkumarg99:gDSlKZHyZgUVjGCW@cluster0.dgxyfnu.mongodb.net/cakepreneur').then(()=>console.log("DB Connected"))
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
